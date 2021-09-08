970040e2c39ffe3276f9ee51d0e8bbebcf7a62d6
dhsj8405 <dhsj8405@naver.com>
2021-09-08 13:48:13 +0900

    douzone_edu_part uploading

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,11 +1,16 @@
 package chapter03;
 
 public class Goods {
+	public static int countOfGoods = 0 ;
 	private static int totalCount = 0;
 	private String name;
 	private int price;
 	private int countStock;
 	private int countSold;
+	
+	public Goods() {
+		countOfGoods++;
+	}
 	public String getName() {
 		return name;
 	}
@@ -16,6 +21,9 @@ public class Goods {
 		return price;
 	}
 	public void setPrice(int price) {
+		if(price<0) {
+			return;
+		}
 		this.price = price;
 	}
 	public int getCountStock() {
@@ -30,5 +38,18 @@ public class Goods {
 	public void setCountSold(int countSold) {
 		this.countSold = countSold;
 	}
+	public void showInfo() {
+		System.out.println(
+				"name:" + name +
+				" , price:" + price+
+				", countStock:" + countStock+ 
+				", countSold:" + countSold);
+		
+	}
+	public int calcDiscountPrice(int percentage) {
+		
+		return price * percentage / 100;
+	}
+
 	
 }
---
+++ practice03/src/main/java/prob01/Member.java
@@ -0,0 +1,27 @@
+package prob01;
+
+public class Member {
+	private String id ;
+	private int point ; 
+	private String name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
