0a6f9ea793d24028afd1e9a728f7fe529faa9ec3
dhsj8405 <dhsj8405@naver.com>
2021-09-15 16:00:43 +0900

    EchoClient,EchoServer

---
+++ network/src/main/java/echo/EchoClient.java
@@ -0,0 +1,71 @@
+package echo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+
+public class EchoClient {
+
+	private static final String SERVER_IP = "127.0.0.1";
+	private static final int SERVER_PORT = 6000;
+	public static void main(String[] args) {
+		Socket socket = null;
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(System.in);
+			
+			socket = new Socket();
+			
+			socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+			log("connected");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));// 개행붙여오는애들 받는것
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);// true 넣으면 자동 fflush 개행 붙여서 보내는것
+			
+			while(true) {
+				System.out.print(">");
+				String line = scanner.nextLine();
+				
+				if("exit".equals(line)){
+					break;
+				}
+				
+				pw.println(line); // 스캐너로 입력할때 개행이 입력안되므로 보내주는것
+				
+				String data = br.readLine();
+				if(data == null) {
+					log("closed by server");
+					break;
+				}
+				System.out.println("<" + data);
+			}
+		}catch(SocketException e) {
+			log("suddenly closed by server");
+		}catch(IOException e) {
+			log("error:" + e);
+		}finally {
+			try {
+				if(scanner != null) {
+					scanner.close();
+				}
+				if(socket != null && socket.isClosed() == false) {
+					socket.close();					
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+
+	}
+	private static void log(String log) {
+		System.out.println("[Echo Client] " + log);
+	}
+
+}
--- network/src/main/java/test/TCPClient.java
+++ network/src/main/java/test/TCPClient.java
@@ -1,6 +1,8 @@
 package test;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.net.SocketException;
@@ -17,6 +19,24 @@ public class TCPClient {
 			//2. 서버 연결
 			socket.connect(new InetSocketAddress(SERVER_IP,SERVER_PORT));
 			
+			// 3. IO Stream 받아오기
+			InputStream is = socket.getInputStream();
+			OutputStream os = socket.getOutputStream();
+
+			// 4. 쓰기
+			String data = "Hello World";
+			os.write(data.getBytes("UTF-8"));//엔코딩
+			
+			//5. 읽기
+			byte[] buffer = new byte[256];
+			int readByteCount = is.read(buffer); // blocking
+			if (readByteCount == -1) {
+				// 반대편(클라이언트)가 정상적으로 종료(close() 호출)
+				System.out.println("[Client] closed by server");
+			}
+			
+			data = new String(buffer, 0, readByteCount, "UTF-8");
+			System.out.println("[client] received:" + data);
 			
 		} catch (SocketException e) {
 			System.out.println("[client] suddenly close by server : " + e);
