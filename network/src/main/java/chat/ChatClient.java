6ae9161abffcdef64fd43dac05f63879ea0e484c
dhsj8405 <dhsj8405@naver.com>
2021-09-23 09:42:41 +0900

    무쓸모 임포트 주석처리

--- network/src/main/java/chat/ChatClient.java
+++ network/src/main/java/chat/ChatClient.java
@@ -5,11 +5,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import java.io.Writer;
+//import java.io.Writer;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.util.List;
+//import java.util.List;
 import java.util.Scanner;
 
 public class ChatClient {
